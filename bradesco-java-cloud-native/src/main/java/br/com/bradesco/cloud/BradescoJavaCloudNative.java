7f92421a2a7dbe268b5403b003e17451c686a47a
Bryan Duarte <albuquerque.bry.n@gmail.com>
2025-04-06 19:22:05 -0300

    feat: first commit.

---
+++ bradesco-java-cloud-native/src/main/java/br/com/bradesco/cloud/BradescoJavaCloudNative.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bradesco.cloud;
+
+/**
+ *
+ * @author developer
+ */
+public class BradescoJavaCloudNative {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
