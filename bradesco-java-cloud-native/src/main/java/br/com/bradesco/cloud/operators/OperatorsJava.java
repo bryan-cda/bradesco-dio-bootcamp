8a851505818e9706ae0619ea640046db1b91e0cb
Bryan Duarte <albuquerque.bry.n@gmail.com>
2025-04-06 19:38:31 -0300

    feat: use assignment operators.

---
+++ bradesco-java-cloud-native/src/main/java/br/com/bradesco/cloud/operators/OperatorsJava.java
@@ -0,0 +1,22 @@
+package br.com.bradesco.cloud.operators;
+
+public class OperatorsJava {
+    public static void main(String[] args){
+        //primitive variables and assignment operators
+       int age = 34;
+       double size = 87.5;
+       float margin = 1.9f;
+       short cm = 174;
+       long cordinates = 19394595;
+       char gender = 'M';
+
+       System.out.println(age);
+       System.out.println(size);
+       System.err.println(margin);
+       System.err.println(cm);
+       System.err.println(cordinates);
+       System.err.println(gender);
+    }   
+
+}
+
